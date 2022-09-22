e5259c2c4e697969f5722df31770db62525fcb0d
oumaima <oumaimaabouelhaitam@gmail.com>
2022-09-22 11:46:13 +0100

    modifier app.js /add function find

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
     const jouer1=document.getElementById("jouer_1");
     const jouer2=document.getElementById("jouer_2");
     const save=document.getElementById("save");
+    const clear=document.getElementById("clear");
     let boxs =document.querySelectorAll(".box");
+    let player1=[];   
+    let player2=[];  
     boxs=Array.from(boxs);
     let tic="X";
     document.getElementById("step2").style.display = 'none';
-
-
-
     save.onclick = function(){
         localStorage.setItem("jouer_1",jouer1.value);
         localStorage.setItem("jouer_2",jouer2.value);
@@ -18,10 +18,15 @@
         document.getElementById("step1").setAttribute("class", "d-none");
         document.getElementById("step2").setAttribute("class", "d-block");
         ;
-
     }
-   
-    const winner =
+
+    // clear.onclick = function()
+    // {
+    //     console.log(document.querySelectorAll(".box").value)
+    //     // document.querySelectorAll(".box").innerHTML="";
+    // }
+
+    let winner =
     [
         [0,1,2],
         [0,4,8],
@@ -35,18 +40,44 @@
     boxs.forEach(function(ps)
     {
         ps.addEventListener("click",function(){
-            if(tic=="X" && ps.innerText.trim()=="")
-            {
-                document.getElementById("player").innerHTML="player is "+localStorage.getItem("jouer_1");
-                ps.innerHTML=tic;
-                tic="O";   
-            }
+              if(tic=="X" && ps.innerText.trim()=="")
+    {
+        document.getElementById("player").innerHTML="player is "+localStorage.getItem("jouer_1");
+        ps.innerHTML=tic;
+        tic="O"; 
+        player1.push(boxs.indexOf(this)); 
+        find(player1,"jouer_1")
+       }
             else if(tic=="O" && ps.innerText.trim()=="")
             {
                 document.getElementById("player").innerHTML="player is "+localStorage.getItem("jouer_2");
                 ps.innerHTML="O";
                 tic="X";
+                player2.push(boxs.indexOf(this)); 
+                find(player2,"jouer_2")
             }
         });
     });
 
+    function find(Player,win){
+        for(var j=0; j<winner.length ; j++)
+        {
+            if(JSON.stringify(Player) === JSON.stringify(winner[j])||JSON.stringify(Player.reverse() )=== JSON.stringify(winner[j]))
+            {
+                document.getElementById("player").innerHTML="winner is "+localStorage.getItem(win);
+                document.getElementById('player').style.backgroundColor = 'green' ; 
+            }
+            if(player1.length>2)
+            {
+                if(JSON.stringify(Player.slice(2)) === JSON.stringify(winner[j])||JSON.stringify(Player.slice(2).reverse()) === JSON.stringify(winner[j]))
+                {
+                    document.getElementById("player").innerHTML="winner is "+localStorage.getItem(win);
+                     document.getElementById('player').style.backgroundColor = 'green' ; 
+                }
+            } 
+        }
+    }
+    
+
+
+ 
