b950b29a226c633b316e0cd06add2285005f7022
oumaima <oumaimaabouelhaitam@gmail.com>
2022-09-26 11:36:40 +0100

    add function clear()

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     const jouer1=document.getElementById("jouer_1");
     const jouer2=document.getElementById("jouer_2");
     const save=document.getElementById("save");
-    const clear=document.getElementById("clear");
+    let clear=document.getElementById("clear");
     let boxs =document.querySelectorAll(".box");
     let player1=[];   
     let player2=[];  
@@ -43,9 +43,23 @@
         [2,8,5],
         [5,2,8],
     ];
+    clear.addEventListener("click",function(){
+       
+        boxs.forEach(function(ps)
+        {
+          ps.innerHTML="";  
+          player1=[];
+          player2=[];
+          tic="X";
+          document.getElementById("player").innerHTML="player is "+localStorage.getItem("jouer_1");
+          document.getElementById('player').style.backgroundColor = '' ; 
+        })
+           
+    });
+
     boxs.forEach(function(ps)
     {
-        ps.addEventListener("click",function(event){
+        ps.addEventListener("click",function(){
             if(tic=="X" && ps.innerText.trim()=="")
             {
                 document.getElementById("player").innerHTML="player is "+localStorage.getItem("jouer_1");
@@ -66,8 +80,10 @@
             }
         });
     });
-
-    var find =function(Player,win){
+    
+     
+    find =function(Player,win)
+    {
         for(let j=0; j<winner.length ; j++)
         {
             if(JSON.stringify(Player) === JSON.stringify(winner[j])||JSON.stringify(Player.reverse() )=== JSON.stringify(winner[j]))
@@ -75,22 +91,17 @@
                 document.getElementById("player").innerHTML="winner is "+localStorage.getItem(win);
                 document.getElementById('player').style.backgroundColor = 'green' ; 
                 tic="";
-              }
-            
+            }
             if(player1.length>2)
             {
                 if(JSON.stringify(Player.slice(2)) === JSON.stringify(winner[j])||JSON.stringify(Player.slice(2).reverse()) === JSON.stringify(winner[j]))
                 {
                     document.getElementById("player").innerHTML="winner is "+localStorage.getItem(win);
-                     document.getElementById('player').style.backgroundColor = 'green' ; 
-                     tic="";
-                       }
+                    document.getElementById('player').style.backgroundColor = 'green' ; 
+                    tic="";
+                }
             } 
-       
         }
-        return false
-
-      
     }
     
 
